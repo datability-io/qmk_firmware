b5c360c20c4e3a57bced37e9beffbb4614c8ce09
Thomas Wang <thomas@zh-wang.me>
2023-07-31 10:26:51 -0700

    add CH_ADDR

--- keyboards/gmmk/pro/rev1/ansi/keymaps/datability/keymap.c
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/datability/keymap.c
@@ -21,8 +21,8 @@ enum custom_keycodes {
     REPO2,
     REPO3,
     REPO4,
-    GITREBASE,
-    GITDELETEBRANCH,
+    REPO5,
+    CH_ADDR,
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -53,15 +53,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             } else {
             }
             break;
-        case GITREBASE:
+        case REPO5:
             if (record->event.pressed) {
-                SEND_STRING("curr_branch_name=$(git rev-parse --abbrev-ref HEAD) && git checkout master && git pull && git checkout $curr_branch_name && git pull origin master\n");
+                SEND_STRING("code ~/src/di-airflow2-test/\n");
             } else {
             }
             break;
-        case GITDELETEBRANCH:
+        case CH_ADDR:
             if (record->event.pressed) {
-                SEND_STRING("curr_branch_name=$(git rev-parse --abbrev-ref HEAD) && if [ -z \"$(git diff head master)\" ]; then\n    git checkout master\n    git branch -D $curr_branch_name\nfi\n");
+                SEND_STRING(KC_LALT("l"));
             } else {
             }
             break;
@@ -93,21 +93,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Since this is, among other things, a "gaming" keyboard, a key combination to enable NKRO on the fly is provided for convenience.
     // Press Fn+N to toggle between 6KRO and NKRO. This setting is persisted to the EEPROM and thus persists between restarts.
     [0] = LAYOUT(
-        KC_ESC,  KC_F1,   KC_F2,   KC_MISSION_CONTROL,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR,          KC_MUTE,
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_DEL,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_PGUP,
-        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_PGDN,
-        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   KC_END,
-        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(1),   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+        KC_ESC,  KC_F1,   KC_F2,           KC_MISSION_CONTROL,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR,          KC_MUTE,
+        KC_GRV,  KC_1,    KC_2,            KC_3,                 KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          REPO1,
+        KC_TAB,  KC_Q,    KC_W,            KC_E,                 KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          REPO2,
+        KC_CAPS, KC_A,    KC_S,            KC_D,                 KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           REPO3,
+        KC_LSFT,          KC_Z,            KC_X,                 KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   REPO4,
+        KC_LCTL, KC_LGUI, KC_LALT,                               KC_SPC,                             KC_RALT, MO(1),   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     [1] = LAYOUT(
-        _______, KC_MYCM, KC_WHOM, KC_CALC, KC_MSEL, KC_MPRV, KC_MNXT, KC_MPLY, KC_MSTP, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______,          _______,
-        _______, REPO1, REPO2, REPO3, REPO4, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
-        _______, RGB_TOG, RGB_VAI, _______, GITREBASE, _______, _______, _______, _______, _______, _______, _______, _______, QK_BOOT,            _______,
-        _______, RGB_HUI, RGB_VAD, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,          _______,
-        _______,          GITDELETEBRANCH, _______, _______, _______, _______, NK_TOGG, _______, _______, _______, _______,          _______, RGB_MOD, _______,
-        _______, _______, _______,                            _______,                            _______, _______, _______, RGB_SPD, RGB_RMOD, RGB_SPI
+        _______, KC_MYCM, KC_WHOM,         KC_CALC,              KC_MSEL, KC_MPRV, KC_MNXT, KC_MPLY, KC_MSTP, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______,          _______,
+        _______, _______, _______,         _______,              _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL,           REPO5,
+        _______, RGB_TOG, RGB_VAI,         _______,              _______, _______, _______, _______, _______, _______, _______, _______, _______, QK_BOOT,          _______,
+        _______, RGB_HUI, RGB_VAD,         _______,              _______, _______, _______, _______, _______, _______, _______, _______,          _______,          _______,
+        _______,          _______,         _______,              _______, _______, _______, NK_TOGG, _______, _______, _______, _______,          _______, RGB_MOD, _______,
+        _______, _______, _______,                               _______,                            _______, _______, _______, RGB_SPD, RGB_RMOD, RGB_SPI
     ),
 
 
