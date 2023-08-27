df7c2891e43907fe0c1bde534fb624f77a495eb9
Thomas Wang <thomas@zh-wang.me>
2023-08-27 09:43:48 -0700

    add quick commit

--- keyboards/gmmk/pro/rev1/ansi/keymaps/datability/keymap.c
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/datability/keymap.c
@@ -28,6 +28,7 @@ enum custom_keycodes {
     CH_ADDR,
     TEST_ALL,
     APP_LIST,
+    QUICK_COMMIT,
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -90,13 +91,19 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             break;
         case TEST_ALL:
             if (record->event.pressed) {
-                SEND_STRING("coverage run -m pytest && coverage report\n");
+                SEND_STRING("coverage run -m pytest && coverage report -m\n");
             } else {
             }
             break;
         case APP_LIST:
             if (record->event.pressed) {
                 SEND_STRING(SS_LCTL(SS_TAP(X_DOWN)));
+            } else {
+            }
+            break;
+        case QUICK_COMMIT:
+            if (record->event.pressed) {
+                SEND_STRING("git commit -m \"quick commit\"\n");
             } else {
             }
             break;
@@ -128,20 +135,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Since this is, among other things, a "gaming" keyboard, a key combination to enable NKRO on the fly is provided for convenience.
     // Press Fn+N to toggle between 6KRO and NKRO. This setting is persisted to the EEPROM and thus persists between restarts.
     [0] = LAYOUT(
-        KC_ESC,  CH_ADDR, KC_F2,           KC_MISSION_CONTROL,   APP_LIST, KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  TEST_ALL,  KC_PSCR,          KC_MUTE,
-        KC_GRV,  KC_1,    KC_2,            KC_3,                 KC_4,     KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,    KC_BSPC,          REPO1,
-        KC_TAB,  KC_Q,    KC_W,            KC_E,                 KC_R,     KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,   KC_BSLS,          REPO2,
-        KC_CAPS, KC_A,    KC_S,            KC_D,                 KC_F,     KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,            KC_ENT,           REPO3,
-        KC_LSFT,          KC_Z,            KC_X,                 KC_C,     KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,            KC_RSFT, KC_UP,   REPO4,
+        KC_ESC,  CH_ADDR, KC_F2,           KC_MISSION_CONTROL,   APP_LIST,      KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  TEST_ALL,  KC_PSCR,          KC_MUTE,
+        KC_GRV,  KC_1,    KC_2,            KC_3,                 KC_4,          KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,    KC_BSPC,          REPO1,
+        KC_TAB,  KC_Q,    KC_W,            KC_E,                 KC_R,          KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,   KC_BSLS,          REPO2,
+        KC_CAPS, KC_A,    KC_S,            KC_D,                 KC_F,          KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,            KC_ENT,           REPO3,
+        KC_LSFT,          KC_Z,            KC_X,                 KC_C,          KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,            KC_RSFT, KC_UP,   REPO4,
         KC_LCTL, KC_LGUI, KC_LALT,                               KC_SPC,                              KC_RALT, MO(1),   KC_RCTL, KC_LEFT, KC_DOWN,   KC_RGHT
     ),
 
     [1] = LAYOUT(
-        _______, KC_MYCM, KC_WHOM,         KC_CALC,              KC_MSEL,  KC_MPRV, KC_MNXT, KC_MPLY, KC_MSTP, KC_MUTE, KC_VOLD, KC_VOLU, _______,   _______,          RGB_TOG,
-        _______, _______, _______,         _______,              _______,  _______, _______, _______, _______, _______, _______, _______, _______,   KC_DEL,           REPO5,
-        _______, RGB_TOG, RGB_VAI,         _______,              _______,  _______, _______, _______, _______, _______, _______, _______, _______,   QK_BOOT,          REPO6,
-        _______, RGB_HUI, RGB_VAD,         _______,              _______,  _______, _______, _______, _______, _______, _______, _______,            _______,          REPO7,
-        _______,          _______,         _______,              _______,  _______, _______, NK_TOGG, _______, _______, _______, _______,            _______, RGB_MOD, REPO8,
+        _______, KC_MYCM, KC_WHOM,         KC_CALC,              KC_MSEL,       KC_MPRV, KC_MNXT, KC_MPLY, KC_MSTP, KC_MUTE, KC_VOLD, KC_VOLU, _______,   _______,          RGB_TOG,
+        _______, _______, _______,         _______,              _______,       _______, _______, _______, _______, _______, _______, _______, _______,   KC_DEL,           REPO5,
+        _______, RGB_TOG, RGB_VAI,         _______,              _______,       _______, _______, _______, _______, _______, _______, _______, _______,   QK_BOOT,          REPO6,
+        _______, RGB_HUI, RGB_VAD,         _______,              _______,       _______, _______, _______, _______, _______, _______, _______,            _______,          REPO7,
+        _______,          _______,         _______,              QUICK_COMMIT,  _______, _______, NK_TOGG, _______, _______, _______, _______,            _______, RGB_MOD, REPO8,
         _______, _______, _______,                               _______,                             _______, _______, _______, RGB_SPD, RGB_RMOD,  RGB_SPI
     ),
 
